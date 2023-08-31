b3e31271bbe067f7efbabd7d73e69e36f6a24b83
joeldelgadogopar2@gmail.com <joeldelgadogopar2@gmail.com>
2023-08-30 23:34:26 -0300

    vym bugfix

--- scripts/vym.js
+++ scripts/vym.js
@@ -10,69 +10,78 @@ const fechas = [];
 var hojaActual = 0;
 
 const closestIndex = (num, arr) => {
-    let curr = arr[0], diff = Math.abs(num - curr);
-    let index = 0;
-    for (let val = 0; val < arr.length; val++) {
-        let newdiff = Math.abs(num - arr[val]);
-        if (newdiff < diff) {
-            diff = newdiff;
-            curr = arr[val];
-            index = val;
-        };
+  let curr = arr[0],
+    diff = Math.abs(num - curr);
+  let index = 0;
+  for (let val = 0; val < arr.length; val++) {
+    let newdiff = Math.abs(num - arr[val]);
+    if (newdiff < diff) {
+      diff = newdiff;
+      curr = arr[val];
+      index = val;
     };
-    return index;
+  };
+  return index;
 };
 
 function dateManager() {
-    fetch(dataUrl)
-        .then(response => response.text())
-        .then(data => {
-            var rows = data.split('\n');
-            //Cargar fechas en el Array
-            for (var i = 1; i < rows.length; i++) {
-                var cells = rows[i].split(',');
-                fechas.push(cells[0]);
-            }
+  fetch(dataUrl)
+    .then(response => response.text())
+    .then(data => {
+      var rows = data.split('\n');
+      //Cargar fechas en el Array
+      for (var i = 1; i < rows.length; i++) {
+        var cells = rows[i].split(',');
+        fechas.push(cells[0]);
+      }
 
-            //Buscar la fecha mas cercana a la actual
-            var date = new Date().getDate();
-            ci = closestIndex(date, fechas);
-            hojaActual = ci;
+      //Buscar la fecha mas cercana a la actual
+      var date = new Date().getDate();
+      ci = closestIndex(date, fechas);
+      hojaActual = ci;
 
-            if (date > fechas[ci] && ci < fechas.length - 1) {
-                hojaActual = ci + 1;
-            } else {
-                hojaActual = ci;
-            };
+      if (date > fechas[ci]) {
 
-            iframeManager();
-        }).catch(error => {
-            console.error("Error al cargar el archivo CSV:", error);
-        });
+        if (ci = fechas.length) {
+          hojaActual = 0;
+        } else {
+          hojaActual = ci + 1;
+        }
+
+      } else {
+        hojaActual = ci;
+      };
+
+      iframeManager();
+    }).catch(error => {
+      console.error("Error al cargar el archivo CSV:", error);
+    });
 
 }
 
 function iframeManager() {
-    iframe = document.getElementById("iframe")
-    iframe.src = hojas[hojaActual]
+  iframe = document.getElementById("iframe");
+
+  iframe.src = hojas[hojaActual];
 
 };
 
 function hojaPlus() {
-    if (hojaActual >= 3) {
-        hojaActual=0;
-    }else{
-      hojaActual++;
-    }
-    iframeManager();
+  if (hojaActual >= 3) {
+    hojaActual = 0;
+  } else {
+    hojaActual++;
+  }
+  iframeManager();
 };
+
 function hojaMinus() {
-    if (hojaActual <= 0) {
-        hojaActual=3;
-    }else{
-      hojaActual--;
-    }
-    iframeManager();
+  if (hojaActual <= 0) {
+    hojaActual = 3;
+  } else {
+    hojaActual--;
+  }
+  iframeManager();
 };
 
 window.addEventListener("load", dateManager);
